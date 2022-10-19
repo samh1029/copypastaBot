1d36aedcbd5cd1e20583363307eb715aed25d6e0
Sam Helman <samhelman@outlook.com>
2022-10-19 19:53:24 +0100

    Spaces

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='-',intents=intents)
 copypastaDict = {}
 
+
 def resetCSV():
     with open('copypastas.csv', mode='r', encoding="utf8") as infile:
         reader = csv.reader(infile)
